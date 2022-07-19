d2ecacd6a0deb528bf217eb09bc64e679c47d858
PTHoang <pthoang@cmcglobal.vn>
2022-07-19 11:59:58 +0700

    test

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "deploying"
+sudo chown -R yllapito:yllapito /home/maintenance/app.war
 /home/maintenance/JenkinsDeployOptimi.sh
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "stop tomcat"
 sudo /home/maintenance/killTomcat.sh
-sudo chown -R yllapito:yllapito /home/maintenance/app.war
