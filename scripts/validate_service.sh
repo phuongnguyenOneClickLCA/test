12719efebd06ec6e6abb7828eb0bd07646165fc7
phuongnguyenOneClickLCA <103567320+phuongnguyenOneClickLCA@users.noreply.github.com>
2022-07-20 16:00:36 +0700

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # verify we can access our webpage successfully
-ps -ef | grep java
-/home/maintenance/restartSoftware.sh
+pkill -9 -f tomcat
+sleep 15
+/opt/tomcat/bin/startup.sh
+sleep 10
 curl -v --silent http://localhost:8080/app/ 2>&1 | grep "HTTP/1.1 200"
