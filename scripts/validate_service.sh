e2969e6fee5de30b371567267bd2aebc35826622
phuongnguyenOneClickLCA <103567320+phuongnguyenOneClickLCA@users.noreply.github.com>
2022-07-20 15:40:20 +0700

    Update validate to check 200

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -3,4 +3,4 @@
 # verify we can access our webpage successfully
 ps -ef | grep java
 /home/maintenance/restartSoftware.sh
-curl -v --silent http://localhost:8080/app/ 2>&1 | grep 200
+curl -v --silent http://localhost:8080/app/ 2>&1 | grep "HTTP/1.1 200"
